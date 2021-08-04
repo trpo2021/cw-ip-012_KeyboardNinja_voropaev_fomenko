6b0fad2eb02f2e9b97784c82cd0d1d6d43a1b152
Eugenetucha <fomenko.woody.list@gmail.com>
2021-08-05 03:06:01 +0700

    Выполненая 5-ая задача

--- src/difficulty.py
+++ src/difficulty.py
@@ -1,13 +1,46 @@
-from tkinter import *
+import time
 from random import choices
+from tkinter import *
+pos = 0
+b = 0
+mistakes = 0
+correct_presses = 0
+def get_words(word_num):
+    with open("words.txt", "r") as words:
+        return choices(list(words), k=word_num)
+def key_type(e):
+    if e.keycode != 16:
+        global pos, b, mistakes, correct_presses
+        if test_text[pos] != e.char:
+            text_widget.tag_add("wrong", f"1.{b}")
+            mistakes += 1
+        if test_text[pos] == e.char:
+            text_widget.tag_add("correct", f"1.{b}")
+            correct_presses += 1
+        b += 1
+        pos += 1
 
+        if pos == len(test_text):
+            rav = time.time() - start_time
+            label1 = Label(root2, text="NICE!", fg="#eee", bg="#957")
+            label2 = Label(root2, text="TIME:", fg="#eee", bg="#957")
+            label0 = Label(root2, text=rav, fg="#eee", bg="#957")
 
+            label1.pack()
+            label2.pack()
+            label0.pack()
+            with open("LeaderBoard.txt", "a") as board:
+                board.writelines(str(rav) + "\n")
+            with open("LeaderBoard.txt", "r") as board:
+                lines = board.readlines()
+                lines.sort()
+                open("sort.txt", "w").write("\n".join(lines))
 def dif():
     global root2
     global test_text
     global text_widget
     root2 = Tk()  
-    root2.deiconify()  
+    root2.deiconify()
 
     text_widget = Text(root2, height=10, width=100,
                        padx=20, pady=20, font=("Arial ", 16))
---
+++ src/main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import commands
+from PIL import ImageTk, Image
+import sys
+from list_pop import list_pip
+root = Tk()  
+top = Toplevel()  
+top.geometry("600x400")
+top.title("Клавиатурный тренажер")  
+var = IntVar()
+Radiobutton(top, text='Легкая', variable=var, value=0).pack()
+Radiobutton(top, text='Нормальная', variable=var, value=1).pack()
+Radiobutton(top, text='Сложная', variable=var, value=2).pack()
+button1 = Button(top, text="Играть", command=lambda: commands.command()).pack()
+button2 = Button(top, text="Выйти", command=lambda: command2()).pack()
+img = ImageTk.PhotoImage(Image.open("images.jpg"))
+panel = Label(top, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+label5 = Label(top, text="Топ времени игроков:", justify=LEFT, font='Times 10')
+label5.place(relx=.1, rely=.1)
+label2 = Label(top, text=list_pip(1), justify=LEFT, font='Times 10')
+label2.place(relx=.1, rely=.2)
+label3 = Label(top, text=list_pip(3), justify=LEFT, font='Times 10')
+label3.place(relx=.1, rely=.3)
+label4 = Label(top, text=list_pip(5), justify=LEFT, font='Times 10')
+label4.place(relx=.1, rely=.4)
+
+
+def start_app():
+    root.withdraw()  
+    return root
+
+
+def command2():
+    top.destroy() 
+    root.destroy()  
+    sys.exit()  
+    
+    
+def command():
+
+    with open("words.txt", "r"):
+        test_words1 = difficulty.get_words(var.get() + 3)
+        app2 = difficulty.dif(test_words1)
+        app2.mainloop()
+    return True
+
+
+if __name__ == '__main__':
+    app = start_app()
+    app.mainloop()
